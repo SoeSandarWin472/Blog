6b2c91c295f63c06373a7819aee0238f776aac51
Soe-sandar-win <soesandarwin472000@gmail.com>
2023-02-09 15:10:10 +0630

    component attributes

--- resources/views/components/single-comment.blade.php
+++ resources/views/components/single-comment.blade.php
@@ -1,5 +1,6 @@
 @props(['comment'])
-<div class="card d-flex p-3 my-3 shadow-sm">
+
+    <x-card-wrapper>
     <div class="d-flex">
         <div>
             <img
@@ -18,4 +19,5 @@
     <p class="mt-1">
         {{$comment->body}}
     </p>
-</div>
+
+</x-card-wrapper>
